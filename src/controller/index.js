bd3e6a3177d4a29dd08ab54a86ebac7400985529
longhui520 <39285837+longhui520@users.noreply.github.com>
2020-09-28 23:55:39 +0800

    项目初始化

---
+++ src/controller/index.js
@@ -0,0 +1,17 @@
+const glob = require("glob")
+const path = require("path")
+
+const controllerPath = new Map()
+const controllerClass = new Map()
+
+class ControllerLoader {
+  constructor(){
+
+  }
+  loadFiles(){
+
+  }
+  getClass(){
+    
+  }
+}
