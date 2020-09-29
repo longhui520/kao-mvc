10de1c8100271e020c3114d06ac4a4b1cdefa0cb
longhui <625243422@qq.com>
2020-09-29 18:09:08 +0800

    update

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,17 +1,26 @@
-const glob = require("glob")
-const path = require("path")
+// const glob = require("glob")
+// const path = require("path")
 
-const controllerPath = new Map()
-const controllerClass = new Map()
+// const controllerPath = new Map()
+// const controllerClass = new Map()
 
-class ControllerLoader {
-  constructor(){
+// class ControllerLoader {
+//   constructor(){
 
-  }
-  loadFiles(){
-
-  }
-  getClass(){
+//   }
+//   loadFiles(target){
+//     return glob.sync(target)
+//   }
+//   getClass(){
     
-  }
-}
+//   }
+// }
+// var c = new ControllerLoader();
+// console.log(c.loadFiles(__dirname));
+const debug = require("debug")("koa-mvc")
+debug('loading item 11');
+const assert = require("assert")
+const b = assert(2>5,'出错 了')
+
+const deprecate = require('depd')('koa-mvc');
+deprecate("已经废弃了")
